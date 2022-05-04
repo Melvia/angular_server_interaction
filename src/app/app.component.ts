df1e121d631881212791321211041be9c64a271d
Руденко Наталья Сергеевна <rudenko@technocom.tech>
2022-05-04 16:39:54 +0500

    POST-request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,38 @@
 import { Component} from '@angular/core';
 import { HttpService} from './http.service';
+import {User} from './user';
     
 @Component({
     selector: 'app-root',
-    template: `<div *ngIf="done">Сумма = {{sum}}</div>
-    <div class="form-group">
-        <label>Введите первое число</label>
-        <input class="form-control" type="number" name="num1" [(ngModel)]="num1" />
-    </div>
-    <div class="form-group">
-        <label>Введите второе число</label>
-        <input class="form-control" type="number" name="num2" [(ngModel)]="num2" />
-    </div>
-    <div class="form-group">
-        <button class="btn btn-default" (click)="submit()">Отправить</button>
-    </div>`,
+    template: `<div class="form-group">
+    <label>Имя</label>
+    <input class="form-control" name="username" [(ngModel)]="user.name" />
+</div>
+<div class="form-group">
+    <label>Возраст</label>
+    <input class="form-control" type="number" name="age" [(ngModel)]="user.age" />
+</div>
+<div class="form-group">
+    <button class="btn btn-default" (click)="submit(user)">Отправить</button>
+</div>
+<div *ngIf="done">
+    <div>Получено от сервера:</div>
+    <div>Имя: {{receivedUser?.name}}</div>
+    <div>Возраст: {{receivedUser?.age}}</div>
+</div>`,
     providers: [HttpService]
 })
 export class AppComponent { 
-  name = "Natali";
-  num1: number = 0;
-  num2: number = 0;
-  sum: number | undefined;
+  user: User=new User("", 0); // данные вводимого пользователя
+       
+  receivedUser: User | undefined; // полученный пользователь
   done: boolean = false;
-  constructor(public httpService: HttpService){}
-  submit(){
-      this.httpService.getSum(this.num1, this.num2).subscribe((data:any) => {
-          this.sum=data.result; 
-          this.done=true;
-      });
+  constructor(private httpService: HttpService){}
+  submit(user: User){
+      this.httpService.postData(user)
+              .subscribe(
+                  (data: any) => {this.receivedUser=data; this.done=true;},
+                  error => console.log(error)
+              );
   }
 }
