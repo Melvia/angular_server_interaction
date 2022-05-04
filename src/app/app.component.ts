c86693a7ce9e5d50911c81ceb13c4a825e495f7b
Руденко Наталья Сергеевна <rudenko@technocom.tech>
2022-05-04 16:04:53 +0500

    Request parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit} from '@angular/core';
-import { HttpService } from './http.service';
-import {User} from './user';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [HttpService]
-})
-export class AppComponent implements OnInit { 
-   
-  users: User[]=[];
-  
-   
-  constructor(public httpService: HttpService){}
+import { Component} from '@angular/core';
+import { HttpService} from './http.service';
     
-  ngOnInit(){
-        
-      this.httpService.getUsers().subscribe((data: User[]) => this.users=data);
-      
+@Component({
+    selector: 'app-root',
+    template: `<div *ngIf="done">Сумма = {{sum}}</div>
+    <div class="form-group">
+        <label>Введите первое число</label>
+        <input class="form-control" type="number" name="num1" [(ngModel)]="num1" />
+    </div>
+    <div class="form-group">
+        <label>Введите второе число</label>
+        <input class="form-control" type="number" name="num2" [(ngModel)]="num2" />
+    </div>
+    <div class="form-group">
+        <button class="btn btn-default" (click)="submit()">Отправить</button>
+    </div>`,
+    providers: [HttpService]
+})
+export class AppComponent { 
+  name = "Natali";
+  num1: number = 0;
+  num2: number = 0;
+  sum: number | undefined;
+  done: boolean = false;
+  constructor(public httpService: HttpService){}
+  submit(){
+      this.httpService.getSum(this.num1, this.num2).subscribe((data:any) => {
+          this.sum=data.result; 
+          this.done=true;
+      });
   }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,26 +1,15 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {User} from './user';
-import {Observable, throwError} from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-  
+import {HttpClient, HttpParams} from '@angular/common/http';
+   
 @Injectable()
 export class HttpService{
-
-    errorMessage: String = "";
-    
+   
     constructor(private http: HttpClient){ }
-        
-    getUsers() : Observable<User[]> {
-        return this.http.get('assets/usersP.json').pipe(map((data:any)=>{
-            let usersList = data["userList"];
-            return usersList.map(function(user: any): User {
-                return new User(user.userName, user.userAge);
-              });
-        }),
-        catchError(err => {console.log(err); this.errorMessage = err.message;
-        return []; //все равно надо вернуть объект Observable
-    }))
-    }; 
+      
+    getSum(num1: number, num2: number){
+        const params = new HttpParams()
+        .set('num1', num1.toString())
+        .set('num2', num2.toString());
+        return this.http.get('http://localhost:3000/sum', {params});
+    }
 }
-
